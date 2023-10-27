fbb0a8650f9e86ce0ee0af6b3f25532f57beffc4
Saro G. <sarog@users.noreply.github.com>
2023-10-27 10:57:11 -0400

    Cleanup Types

--- utils.go
+++ utils.go
@@ -1,14 +1,11 @@
 package jetrmm
 
-import (
-	"os"
-)
-
-func FileExists(path string) bool {
+// moved to rmm-nats-api
+/*func FileExists(path string) bool {
 	if _, err := os.Stat(path); err != nil {
 		if os.IsNotExist(err) {
 			return false
 		}
 	}
 	return true
-}
+}*/
